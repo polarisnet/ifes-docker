8f4b850ec989121d52a81503f7967ea6c494e097
Ferdie Putrawan <ferdie.putrawan@polarisnet.com.my>
2017-05-09 21:43:13 +0700

    Windows Friendly
    Modify docker script to be windows host friendly

--- start.sh
+++ start.sh
@@ -1,3 +1,17 @@
-service php7.0-fpm start
+#!/bin/bash
 
-service nginx start
+# Start the first process
+/etc/startphpfpm -D
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start startphpfpm: $status"
+  exit $status
+fi
+
+# Start the second process
+/etc/startnginx -D
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start startnginx: $status"
+  exit $status
+fi
